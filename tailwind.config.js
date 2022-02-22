8ba11ddb30559461892ed4adba1c5b5a17d8a2f4
ytskk <thefrozenewr@gmail.com>
2022-02-22 21:14:08 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "background": "#F5E9E0",
+        "primary": "#E93B32",
+        "secondary": "#F6DA01",
+      }
+    },
+  },
+  plugins: [],
+}
